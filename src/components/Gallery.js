8b48474cc8e641dd8749d56ee110ff3097ff5f16
Prakhar <ptofficial29@gmail.com>
2022-06-01 00:32:03 +0530

    image urls added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,16 +5,68 @@ import 'react-image-gallery/styles/css/image-gallery.css'
 const Gallery = () => {
     const images = [
         {
-          original: 'https://picsum.photos/id/1018/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1018/250/150/',
+          original: 'https://images2.imgbox.com/af/f6/FpUICC53_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/af/f6/FpUICC53_t.jpeg',
         },
         {
-          original: 'https://picsum.photos/id/1015/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1015/250/150/',
+          original: 'https://images2.imgbox.com/63/c3/hG9G48aV_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/63/c3/hG9G48aV_t.jpeg',
         },
         {
-          original: 'https://picsum.photos/id/1019/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1019/250/150/',
+          original: 'https://images2.imgbox.com/a6/48/Xy6lClZR_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/a6/48/Xy6lClZR_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/b4/e6/kLgJn1LB_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/b4/e6/kLgJn1LB_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/93/9c/OZIHLCkb_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/93/9c/OZIHLCkb_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/7f/fe/PWlPGEBE_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/7f/fe/PWlPGEBE_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/cb/4b/SWvzOVac_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/cb/4b/SWvzOVac_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/a7/47/BLGQtYwB_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/a7/47/BLGQtYwB_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/8b/b9/TA3pKYzM_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/8b/b9/TA3pKYzM_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/6b/e2/EBWffkst_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/6b/e2/EBWffkst_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/f7/42/fEumDluM_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/f7/42/fEumDluM_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/71/aa/mLpdD7Sn_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/71/aa/mLpdD7Sn_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/cc/7b/9JIB4RC0_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/cc/7b/9JIB4RC0_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/90/ba/EycNHo9a_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/90/ba/EycNHo9a_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/f4/0c/fOOYx44M_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/f4/0c/fOOYx44M_t.jpeg',
+        },
+        {
+          original: 'https://images2.imgbox.com/2d/17/gZlN3kjA_o.jpeg',
+          thumbnail: 'https://thumbs2.imgbox.com/2d/17/gZlN3kjA_t.jpeg',
         },
       ];
 
